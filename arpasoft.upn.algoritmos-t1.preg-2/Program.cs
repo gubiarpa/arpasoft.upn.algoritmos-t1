f0277ed51876a8ea3d3555fcd40cf7a41dab1b8a
Billy Arredondo <billy.arredondo@pucp.pe>
2023-04-02 14:09:06 -0500

    pregunta 2

---
+++ arpasoft.upn.algoritmos-t1.preg-2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+bool EsVocal(char caracter)
+{
+    var arrVocales = new char[] { 'a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú' };
+
+    for (int i = 0; i < arrVocales.Length; i++)
+    {
+        if (caracter == arrVocales[i])
+            return true;
+    }
+
+    return false;
+}
+
+int CuentaVocales(string cadena)
+{
+    int contador = 0;
+    var arrCaracteres = cadena.ToCharArray();
+
+    for (int i = 0; i < arrCaracteres.Length; i++)
+    {
+        var caracter = arrCaracteres[i];
+        if (EsVocal(caracter))
+            contador++;
+    }
+
+    return contador;
+}
+
+var cadenasDePrueba = new string[] { "Perú", "Colombia", "Chile", "Ecuador" };
+Console.WriteLine("*** Cantidad de vocales ***");
+for (int i = 0; i < cadenasDePrueba.Length; i++)
+{
+    var cadena = cadenasDePrueba[i];
+    Console.WriteLine($"- {cadena}: { CuentaVocales(cadena.ToLower()) }");
+}
